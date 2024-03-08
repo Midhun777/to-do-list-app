f663aa92ee3f517a7c0110b090df19e57aa5445a
Midhun777 <midmat4828@gmail.com>
2024-03-08 12:46:31 +0530

    logical part

--- script.js
+++ script.js
@@ -30,4 +30,7 @@ listContainer.addEventListener('click', function(e) {
 	if (e.target.tagName === "LI") {
 		e.target.classList.toggle('checked');
 	}
+	else if (e.target.tagName === "SPAN") {
+		e.target.parentElement.remove();
+	}
 }, false)
